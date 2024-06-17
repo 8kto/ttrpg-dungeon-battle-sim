88122351bc4051189d3801a9fedd3864a73a28a5
8kto <ifhtagn@gmail.com>
2024-06-18 01:37:27 +0200

    Introduce euristics for best class matching

--- src/js/shared/character.js
+++ src/js/shared/character.js
@@ -63,38 +63,6 @@ export const getNewCharacterModifiers = () => {
 }
 
 /**
- * @param {CharacterStats} stats
- * @param {'PrimeAttr' | 'StrictAttr'} kind
- */
-export const getClassSuggestionsXXX = (stats, kind) => {
-  if (kind !== 'PrimeAttr') {
-    throw new Error('Not implemented')
-  }
-
-  const validAttrs = []
-  const matchingClasses = []
-
-  Object.values(AttrScore).forEach((attrName) => {
-    if (stats[attrName].Score >= PRIME_ATTR_MIN) {
-      validAttrs.push(attrName)
-    }
-  })
-
-  Object.entries(characterClasses).forEach(([className, classDef]) => {
-    const isMatching = classDef.PrimeAttr.every(([primeAttrName]) => {
-      return validAttrs.includes(primeAttrName)
-    })
-
-    if (isMatching) {
-      matchingClasses.push(className)
-    }
-  })
-
-  return [validAttrs, matchingClasses]
-}
-
-/**
- * TODO sort attrs, choose the highest
  * TODO if all below 13, suggest a class for the highest attr
  * @param {CharacterStats} stats
  * @param {'PrimeAttr' | 'StrictAttr'} kind
@@ -104,52 +72,67 @@ export const getClassSuggestions = (stats, kind) => {
   if (kind !== 'PrimeAttr') {
     throw new Error('Not implemented')
   }
-  console.log(stats)
   const validAttrs = getValidAttributes(stats, PRIME_ATTR_MIN)
+  const res = getMatchingClasses(validAttrs)
 
-  return getMatchingClasses(validAttrs)
+  if (!res.length) {
+    const sortedAttrs = getSortedAttributes(stats)
+    console.log(sortedAttrs)
+  }
+
+  return res
 }
 
 /**
  * @param {CharacterStats} stats
  * @param {number} minScore
- * @returns {string[]} Valid attribute names
+ * @returns {Array<[AttrScore, number]>} Valid attribute names
  */
 export const getValidAttributes = (stats, minScore) => {
   return Object.entries(stats)
     .filter(([key, val]) => !!val.Score && !!AttrScore[key] && val.Score >= minScore)
     .sort((a, b) => b[1].Score - a[1].Score)
+    .map(([name, { Score }]) => [name, Score])
+}
+
+/**
+ * @param {CharacterStats} stats
+ * @returns {Array<AttrScore>} Attribute names sorted by theis scores
+ */
+export const getSortedAttributes = (stats) => {
+  return Object.entries(stats)
+    .filter(([key, val]) => !!val.Score && !!AttrScore[key])
+    .sort((a, b) => b[1].Score - a[1].Score)
     .map(([key]) => key)
 }
 
 /**
- * @param {string[]} validAttrs
- * @returns {string[]} Matching class names
+ * @param {Array<[AttrScore, number]>} validAttrs
+ * @returns {Array<[CharacterClass, number]>} Matching class names
  */
-const getMatchingClasses = (validAttrs) => {
-  return Object.entries(characterClasses).reduce((matchingClasses, [className, classDef]) => {
+export const getMatchingClasses = (validAttrs) => {
+  const targetAttrs = Object.fromEntries(validAttrs)
+  const matching = Object.entries(characterClasses).reduce((matchingClasses, [className, classDef]) => {
     const isMatching = classDef.PrimeAttr.every(([primeAttrName]) => {
-      return validAttrs.includes(primeAttrName)
+      return !!targetAttrs[primeAttrName]
     })
 
     if (isMatching) {
-      matchingClasses.push(className)
+      matchingClasses.push([className, classDef.PrimeAttr, targetAttrs])
     }
 
     return matchingClasses
   }, [])
+
+  return matching
 }
 
 /**
- * @param {Array<CharacterClass>} suggestedClasses
- * @returns {CharacterClass|null}
+ * @deprecated To be reworked
+ * @returns {CharacterClass}
  */
-export const getRandomClass = (suggestedClasses) => {
-  if (!suggestedClasses.length) {
-    return null
-  }
-
-  return characterClasses[getRandomArrayItem(suggestedClasses)]
+export const getRandomClass = () => {
+  return getRandomArrayItem(Object.values(characterClasses))
 }
 
 /**
@@ -180,3 +163,80 @@ export const getCharHitPoints = (charClass, stats) => {
 // TODO saving Throw / num + details
 // TODO generate with strict 0e attrs
 // TODO Fighter Parrying Ability
+
+/**
+ * @typedef {[CharacterClass, [AttrScore, number][], Record<AttrScore, number>]} SuggestedClassRecord
+ * @typedef {SuggestedClassRecord[]} SuggestedClassData
+ */
+
+/**
+ * @param {SuggestedClassData} data
+ * @returns {CharacterClass}
+ */
+export const getBestClass = (data) => {
+  if (data.length === 0) {
+    return null
+  }
+
+  // Find the record(s) with the longest classPrimeAttrs
+  const maxPrimeAttrLength = Math.max(...data.map(([, classPrimeAttrs]) => classPrimeAttrs.length))
+  const longestPrimeAttrs = data.filter(([, classPrimeAttrs]) => classPrimeAttrs.length === maxPrimeAttrLength)
+
+  // Find the record(s) with the highest sum of characterAttrScores
+  const getPropsSum = (scores) => Object.values(scores).reduce((sum, score) => sum + score, 0)
+  const maxAttrScoreSum = Math.max(...longestPrimeAttrs.map(([, , charAttrScores]) => getPropsSum(charAttrScores)))
+  const bestMatches = longestPrimeAttrs.filter(
+    ([, , charAttrScores]) => getPropsSum(charAttrScores) === maxAttrScoreSum,
+  )
+
+  // If there are multiple best matches, choose one randomly
+  if (bestMatches.length > 1) {
+    console.info(
+      'Choosing random from best matches',
+      bestMatches.map(([name]) => name),
+    )
+
+    return getRandomArrayItem(bestMatches)[0]
+  }
+
+  return bestMatches[0][0]
+}
+
+//
+// // Example usage with the provided data structure
+// const data = [
+//   [
+//     'Fighter',
+//     [['Strength', 13]],
+//     {
+//       Intelligence: 14,
+//       Strength: 13,
+//     },
+//   ],
+//   [
+//     'MagicUser',
+//     [['Intelligence', 13]],
+//     {
+//       Intelligence: 14,
+//       Strength: 13,
+//     },
+//   ],
+//   [
+//     'Paladin',
+//     [['Strength', 13]],
+//     {
+//       Intelligence: 14,
+//       Strength: 13,
+//     },
+//   ],
+//   [
+//     'Ranger',
+//     [['Strength', 13]],
+//     {
+//       Intelligence: 14,
+//       Strength: 13,
+//     },
+//   ],
+// ]
+//
+// console.log(getBestClass(data))
