e3470b60aa8f4e424933b5f965a0d7d2f49270b0
8kto <ifhtagn@gmail.com>
2024-06-25 23:34:21 +0200

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,17 +8,13 @@ else
   exit 1
 fi
 
-# Now you can use the environment variables in your script
-echo "DEPLOY_SSH_PORT: $DEPLOY_SSH_PORT"
-echo "DEPLOY_SSH_HOST: $DEPLOY_SSH_HOST"
-
 # Get the current git branch
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 CURRENT_TIMESTAMP=$(date +%s)
 
 # Check if current branch is 'main'
 if [ "$CURRENT_BRANCH" != "main" ]; then
-    DEPLOY_SSH_HOST=$DEPLOY_SSH_HOST/beta
+    DEPLOY_SSH_HOST="$DEPLOY_SSH_HOST"beta
 fi
 
 mkdir -p /tmp/snw-to-be-deployed
@@ -28,4 +24,6 @@ cp -r src/* /tmp/snw-to-be-deployed
 find /tmp/snw-to-be-deployed \( -name '*.html' -or -name '*.js' \) -exec sed -i "s/\$VERSION\\$/$CURRENT_TIMESTAMP/g" {} +
 
 # Deploy to remote server
-echo scp -P $DEPLOY_SSH_PORT -r /tmp/snw-to-be-deployed/* $DEPLOY_SSH_HOST
+echo "DEPLOY_SSH_PORT: $DEPLOY_SSH_PORT"
+echo "DEPLOY_SSH_HOST: $DEPLOY_SSH_HOST"
+scp -P $DEPLOY_SSH_PORT -r /tmp/snw-to-be-deployed/* $DEPLOY_SSH_HOST
