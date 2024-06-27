eabc2db6ad046943fe7a53c2e8c4f5361974897c
8kto <ifhtagn@gmail.com>
2024-06-28 00:55:50 +0200

    Convert to TS

---
+++ src/js/shared/dice.ts
@@ -0,0 +1,54 @@
+export enum Dice {
+  d4 = 4,
+  d6 = 6,
+  d8 = 8,
+  d10 = 10,
+  d12 = 12,
+  d20 = 20,
+  d100 = 100,
+}
+
+export type DiceKind = keyof typeof Dice
+export type DiceSides = (typeof Dice)[DiceKind]
+
+/**
+ * Generates a cryptographically secure random integer between min (inclusive) and max (inclusive)
+ */
+export const secureRandomInteger = (min: number, max: number) => {
+  const range = max - min + 1
+  const maxUint32 = 0xffffffff
+  const limit = maxUint32 - (maxUint32 % range)
+  let randomValue
+
+  do {
+    randomValue = crypto.getRandomValues(new Uint32Array(1))[0]
+  } while (randomValue >= limit)
+
+  return min + (randomValue % range)
+}
+
+export const roll = (dice = Dice.d100): number => secureRandomInteger(1, dice)
+
+export const rollDiceFormula = (formula: string): number => {
+  const [numDice, numSides] = formula.split('d').map(Number)
+
+  if (isNaN(numDice) || isNaN(numSides)) {
+    throw new Error('Invalid dice formula')
+  }
+
+  let total = 0
+
+  for (let i = 0; i < numDice; i++) {
+    total += roll(numSides)
+  }
+
+  return total
+}
+
+export const getRandomArrayItem = <T = unknown>(arr: Array<T>): T => {
+  if (!arr.length) {
+    throw new Error('Empty array')
+  }
+
+  return arr[secureRandomInteger(0, arr.length - 1)]
+}
