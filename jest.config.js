61dfd8ec62fc2ebf833bb00830a94fc787bf70b7
8kto <ifhtagn@gmail.com>
2024-06-27 23:54:12 +0200

    Drop custom resolver for jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   moduleFileExtensions: ['js'],
-  resolver: '<rootDir>/jest.custom-resolver.js',
   roots: ['<rootDir>/src/js'],
   testEnvironment: 'jsdom',
   testMatch: ['**/*.test.js'],
