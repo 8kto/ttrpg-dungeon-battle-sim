859f73eafd3ca0ba9049aa656893b61ef80cf6c7
8kto <ifhtagn@gmail.com>
2024-06-28 21:52:19 +0200

    Autoformat

--- src/js/domain/CharacterStats.ts
+++ src/js/domain/CharacterStats.ts
@@ -1,42 +1,42 @@
 export type StrengthModifierDef = {
-  Damage: number;
-  ToHit: number;
-  Doors: string;
-  Carry: number;
-};
+  Damage: number
+  ToHit: number
+  Doors: string
+  Carry: number
+}
 
 export type DexterityModifierDef = {
   MissilesToHit: number
   ArmorClass: number
-};
+}
 
 export type ConstitutionModifierDef = {
-   HitPoints: number
-    RaiseDeadSurvivalChance: string
-};
+  HitPoints: number
+  RaiseDeadSurvivalChance: string
+}
 
 export type IntelligenceModifierDef = {
-   MaxAdditionalLanguages: number
- MaxSpellLevel: number
- NewSpellUnderstandingChance: string
- SpellsPerLevel: string
-};
+  MaxAdditionalLanguages: number
+  MaxSpellLevel: number
+  NewSpellUnderstandingChance: string
+  SpellsPerLevel: string
+}
 
 export type WisdomModifierDef = {
-  Score: number;
-};
+  Score: number
+}
 
 export type CharismaModifierDef = {
   MaxNumberOfSpecialHirelings: number
-};
+}
 
 export type CharacterStats = {
-  Strength: StrengthModifierDef;
-  Dexterity: DexterityModifierDef;
-  Constitution: ConstitutionModifierDef;
-  Intelligence: IntelligenceModifierDef;
-  Wisdom: WisdomModifierDef;
-  Charisma: CharismaModifierDef;
-  Gold: number;
-  HitPoints: number;
-};
+  Strength: StrengthModifierDef
+  Dexterity: DexterityModifierDef
+  Constitution: ConstitutionModifierDef
+  Intelligence: IntelligenceModifierDef
+  Wisdom: WisdomModifierDef
+  Charisma: CharismaModifierDef
+  Gold: number
+  HitPoints: number
+}
