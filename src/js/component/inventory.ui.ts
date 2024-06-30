aa25897d16146f1b6462038f169a6a8d46d7dbfd
8kto <ifhtagn@gmail.com>
2024-06-30 19:09:25 +0200

    Rename and split inventory layour utils

--- src/js/component/inventory.ui.ts
+++ src/js/component/inventory.ui.ts
@@ -1,24 +1,12 @@
 import { EquipSets } from '../config/snw/EquipSets'
 import { EquipItem } from '../domain/Equipment'
-import { CharacterClassDef } from '../domain/snw/CharacterClass'
-import { CharacterStats } from '../domain/snw/CharacterStats'
-import { BaseMovementRate } from '../domain/snw/Movement'
 import { getState } from '../state/State'
 import { getEquipNameSuffix } from '../utils/equipment'
 import { dispatchEvent } from '../utils/event'
 import { getInventoryIdFromName } from '../utils/inventory'
-import { getBaseMovementRate, getUndergroundSpeed } from '../utils/snw/movement'
-
-/**
- * @param {string} htmlString Should enclose the layout with one element (div, span etc.)
- * @returns {ChildNode}
- */
-export const createElementFromHtml = <T = HTMLElement>(htmlString: string): T => {
-  const div = document.createElement('div') as T
-  div.innerHTML = htmlString.trim()
-
-  return div.firstChild
-}
+import { getBaseMovementRate } from '../utils/snw/movement'
+import { createElementFromHtml } from '../utils/layout'
+import { renderCharacterSection, updateSpeedDisplay } from './character.ui'
 
 const getInventoryTable = (inventoryId: string): string => {
   return `<table id="${inventoryId}-table-container" class="min-w-full bg-white shadow-md rounded my-4">
@@ -194,97 +182,6 @@ export const markSelectedInventory = (inventoryId: string): void => {
   containerTitle.textContent = getState().getInventory(inventoryId).name
 }
 
-export const scrollToElement = (element: HTMLElement): void => {
-  if (element instanceof HTMLElement) {
-    element.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'nearest',
-    })
-  } else {
-    console.error('Invalid element passed to scrollToElement function.')
-  }
-}
-
-export const renderCasterDetails = (
-  container: HTMLElement,
-  classDef: CharacterClassDef,
-  stats: CharacterStats,
-): void => {
-  const intelligenceAttr = stats.Intelligence
-  const magicUserProps = ['NewSpellUnderstandingChance', 'SpellsPerLevel']
-  const ignoredProps = ['MaxAdditionalLanguages', 'Score']
-
-  const getDetailsItem = (key, value): HTMLElement => {
-    const elem = document.createElement('p')
-    const formatted = key.replace(/([A-Z])/g, ' $1').trim()
-
-    elem.innerHTML = `${formatted}: <span class="text-alt">${value}</span>`
-
-    return elem
-  }
-
-  Object.entries(intelligenceAttr).forEach(([key, value]) => {
-    if (ignoredProps.includes(key)) {
-      return false
-    }
-    if (classDef.name !== 'MagicUser' && magicUserProps.includes(key)) {
-      return false
-    }
-
-    container.appendChild(getDetailsItem(key, value))
-  })
-
-  const spellsNum = classDef.name === 'Cleric' && stats.Wisdom.Score >= 15 ? 1 : classDef.$spellsAtTheFirstLevel
-  container.appendChild(getDetailsItem('Spells at the 1st level', spellsNum))
-
-  container.removeAttribute('hidden')
-}
-
-export const renderStatsContainer = (
-  container: HTMLElement,
-  stats: CharacterStats,
-  classDef: CharacterClassDef,
-): void => {
-  const template = document.getElementById('template-stats') as HTMLTemplateElement
-  const clone = document.importNode(template.content, true)
-
-  const tableStats = clone.querySelector('table.table-stats')
-  const tableBonuses = clone.querySelector('table.table-bonuses')
-
-  Object.entries(stats).forEach(([statName, stat]) => {
-    const { Score, ...bonuses } = stat
-
-    // Attribute scores
-    const statCell = tableStats.querySelector(`.col-stat-${statName} td:nth-child(2)`)
-    if (statCell) {
-      statCell.textContent = Score
-    }
-
-    // Attribute bonuses
-    Object.entries(bonuses).forEach(([bonusName, bonus]) => {
-      const bonusCell = tableBonuses.querySelector(`.col-bonus-${bonusName} td:nth-child(2)`)
-
-      if (bonusCell) {
-        bonusCell.textContent = bonus.toString()
-      }
-    })
-  })
-
-  if (classDef.$isCaster) {
-    const casterDetailsContainer = clone.querySelector<HTMLElement>('.char-stats--caster-details')
-    renderCasterDetails(casterDetailsContainer, classDef, stats)
-  }
-
-  // Other details
-  clone.querySelector('.char-gold').textContent = stats.Gold.toString()
-  clone.querySelector('.char-hp').textContent = stats.HitPoints.toString()
-  clone.querySelector('.char-hd').textContent = classDef.HitDice.toString()
-  clone.querySelector('.char-class').textContent = classDef.name
-
-  container.appendChild(clone)
-}
-
 export const renderErrorMessage = (errorMessage: string): void => {
   const pageContentElement = document.querySelector('.page-content')
   const errorMsgElement = document.createElement('div')
@@ -294,18 +191,6 @@ export const renderErrorMessage = (errorMessage: string): void => {
   pageContentElement.classList.add('text-center')
   pageContentElement.innerHTML = ''
   pageContentElement.appendChild(errorMsgElement)
-}
-
-/**
- * Updates the HTML element with speeds for walking, running, and combat
- * based on the base movement rate.
- */
-export const updateSpeedDisplay = (inventoryId: string, baseMovementRate: BaseMovementRate): void => {
-  const speeds = getUndergroundSpeed(baseMovementRate)
-  document.getElementById(`${inventoryId}-speed-feet-per-turn`).innerHTML =
-    `Walking: <span class="text-alt">${speeds.walking}</span>` +
-    ` • Running: <span class="text-alt">${speeds.running}</span>` +
-    ` • Combat: <span class="text-alt">${speeds.combat}</span>`
 }
 
 /**
@@ -428,17 +313,6 @@ export const renderInventories = (): void => {
         document.querySelector(`#${inventory.id}-inventory-controls-top-section`).classList.add('hidden')
       }
     })
-}
-
-export const renderCharacterSection = (
-  inventoryId: string,
-  charClass: CharacterClassDef,
-  charStats: CharacterStats,
-): void => {
-  const container = document.querySelector<HTMLElement>(`#${inventoryId}-container .char-stats`)
-
-  container.innerHTML = ''
-  renderStatsContainer(container, charStats, charClass)
 }
 
 const addEquipRow = (tableBody: HTMLTableSectionElement, item: EquipItem): void => {
