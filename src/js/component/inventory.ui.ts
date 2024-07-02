b2d10c44d041906aee49e4669198b547e35b17ed
8kto <ifhtagn@gmail.com>
2024-07-02 23:23:13 +0200

    Render random character on new inventory add

--- src/js/component/inventory.ui.ts
+++ src/js/component/inventory.ui.ts
@@ -383,6 +383,7 @@ const bindInventoryCommonControls = (): void => {
 
     getState().setCurrentInventoryId(inventoryId)
     markSelectedInventory(inventoryId)
+    dispatchEvent('RenderNewRandomCharacter')
   })
 }
 
