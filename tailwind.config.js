ce68509175f859b9a3fa095e171313921616a6bb
8kto <ifhtagn@gmail.com>
2024-07-03 00:46:41 +0200

    Install Tailwind CSS as a package

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts}"],
+  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        gen: {
+          50: '#f5f4f1',
+          100: '#e6e2db',
+          200: '#cec7ba',
+          300: '#b2a592',
+          400: '#9e8c77',
+          500: '#8d7965',
+          600: '#786456',
+          700: '#614f47',
+          800: '#54453f',
+          900: '#4a3d39',
+          950: '#29201f',
+        },
+      },
+      fontSize: {
+        xxs: '0.625rem',
+        xxxs: '0.5rem',
+      },
+    },
+  },
+}
