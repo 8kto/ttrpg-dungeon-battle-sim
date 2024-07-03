3345becaf6b7fe07199ca56e5c0b8733f1e02be6
8kto <ifhtagn@gmail.com>
2024-07-03 21:40:27 +0200

    Add EXP utils

---
+++ src/js/utils/snw/experience.ts
@@ -0,0 +1,23 @@
+// Wisdom 13+: all chars (cleric gets both)
+// Charisma 13+: all chars
+
+import { PRIME_ATTR_MIN } from '../../config/snw/CharacterClasses'
+import { AttrScore, CharacterClassDef } from '../../domain/snw/CharacterClass'
+import { CharacterStats } from '../../domain/snw/CharacterStats'
+
+const BASE_BONUS_VALUE = 5
+
+export const getExperienceBonus = (charDef: CharacterClassDef, stats: CharacterStats): number => {
+  const primeAttributes = [AttrScore.Wisdom, AttrScore.Charisma]
+  const baseBonus = primeAttributes.reduce((bonus, attr) => {
+    return bonus + (stats[attr].Score >= PRIME_ATTR_MIN ? BASE_BONUS_VALUE : 0)
+  }, 0)
+
+  const hasClassBonus = charDef.PrimeAttr.every((cur) => {
+    const [attrScore] = cur
+
+    return attrScore in stats && stats[attrScore].Score >= PRIME_ATTR_MIN
+  })
+
+  return baseBonus + (hasClassBonus ? BASE_BONUS_VALUE : 0)
+}
