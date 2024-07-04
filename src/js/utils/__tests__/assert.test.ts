307e064e58a77cc08c4a4b45ae41beffc88177e0
8kto <ifhtagn@gmail.com>
2024-07-04 13:03:23 +0200

    Extend test cases with comparison

--- src/js/utils/__tests__/assert.test.ts
+++ src/js/utils/__tests__/assert.test.ts
@@ -6,6 +6,12 @@ describe('assert', () => {
     expect(() => assert(input, 'Var is falsy')).toThrowError('Var is falsy')
   })
 
+  // eslint-disable-next-line no-undefined
+  it.each([false, null, undefined, 0, '', NaN])('should throw for falsy values comparison [%j]', (input) => {
+    // @ts-ignore
+    expect(() => assert(input > 0, 'Var is not greater than 0')).toThrowError('Var is not greater than 0')
+  })
+
   it.each([{}, [], 1, true, 'false', '0', Symbol('X'), new Set()])(
     'should not throw for truthy value [%j]',
     (input) => {
