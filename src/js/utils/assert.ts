2a7d73ffd400bd1912c34a4da74e1665785c06bf
8kto <ifhtagn@gmail.com>
2024-07-07 16:10:38 +0200

    Autoformat

--- src/js/utils/assert.ts
+++ src/js/utils/assert.ts
@@ -1,6 +1,8 @@
-export const assert: <T>(variable: T | undefined | null, msg?: string) => asserts variable is T =
-  <T>(variable: T | undefined | null, msg: string = 'Not defined'): asserts variable is T => {
-    if (!variable) {
-      throw new Error(msg)
-    }
+export const assert: <T>(variable: T | undefined | null, msg?: string) => asserts variable is T = <T>(
+  variable: T | undefined | null,
+  msg: string = 'Not defined',
+): asserts variable is T => {
+  if (!variable) {
+    throw new Error(msg)
   }
+}
