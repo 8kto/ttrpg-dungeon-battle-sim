e57c0d500b4a9e998c3499c1a04d525a5782b4c8
8kto <ifhtagn@gmail.com>
2024-07-18 01:01:20 +0200

    Add missile/melee flags

--- src/js/domain/Equipment.ts
+++ src/js/domain/Equipment.ts
@@ -14,6 +14,8 @@ export enum InventoryItemFlag {
   TWO_HANDED = 0b000001,
   VAR_HANDED = 0b000010,
   MELEE_AND_MISSILE = 0b000100,
+  TYPE_MELEE = 0b001000,
+  TYPE_MISSILE = 0b010000,
 }
 
 export type EquipSet = {
