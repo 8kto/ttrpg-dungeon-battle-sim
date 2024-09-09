8c187b28e6293341c83b438355e4744daab71b15
8kto <ifhtagn@gmail.com>
2024-09-09 23:59:31 +0200

    Refactor modal

--- src/js/component/modal.ts
+++ src/js/component/modal.ts
@@ -1,93 +1,146 @@
-const modalActionButtons = {
+import { createElementFromHtml } from '../utils/layout'
+
+type InputField = {
+  name: string
+  title: string
+  placeholder?: string
+  defaultValue?: string | number
+  valueType?: 'string' | 'number' // Defaults to 'string'
+}
+
+type ConfirmModalProps = {
+  title?: string
+  message?: string
+  type?: 'confirm' | 'modal' // Defaults to 'confirm'
+  fields?: InputField[]
+}
+
+type PromptModalProps = {
+  title?: string
+  message?: string
+  type: 'prompt'
+  fields: InputField[]
+}
+
+type ModalProps = ConfirmModalProps | PromptModalProps
+type ShowModalFn = <T = boolean | Record<string, string>>(props: ModalProps) => Promise<T>
+
+enum ReturnValue {
+  initialValue = 'initialValue',
+  cancel = 'cancel',
+  ok = 'ok',
+}
+
+const modalActionButtons: Record<string, string> = {
   confirm: 'Confirm',
   default: 'OK',
   modal: 'OK',
   prompt: 'OK',
 }
 
-type ConfirmModalProps = {
-  title: string
-  message: string
-  type?: 'confirm' | 'modal' // Defaults to 'confirm'
-  fields?: { name: string; title: string; placeholder?: string }[]
+const createInputFields = (fields: InputField[], container: HTMLElement): void => {
+  fields.forEach(({ defaultValue, name, placeholder, title, valueType }) => {
+    // Use the createElementFromHtml utility to create the label and input structure
+    const label = createElementFromHtml<HTMLLabelElement>(`
+      <label class="form-control w-full">
+        <div class="label">
+          <span class="label-text">${title}</span>
+        </div>
+        <input
+          type="${valueType === 'number' ? 'number' : 'text'}"
+          class="input input-bordered w-full"
+          name="${name}"
+          placeholder="${placeholder ?? ''}"
+          required
+        />
+      </label>
+    `)
+
+    if (typeof defaultValue !== 'undefined') {
+      const input = label.querySelector('input')!
+      input.value = defaultValue.toString()
+    }
+
+    container.appendChild(label)
+  })
 }
 
-type PromptModalProps = {
-  title: string
-  message: string
-  type: 'prompt'
-  fields: { name: string; title: string; placeholder?: string }[]
+const extractFormData = (form: HTMLFormElement): Record<string, string> => {
+  return Object.fromEntries(new FormData(form)) as Record<string, string>
 }
 
-type ModalProps = ConfirmModalProps | PromptModalProps
-
-type ShowModal = (props: ModalProps) => Promise<boolean | Record<string, string>>
-
-export const showModal: ShowModal = async ({ fields, message, title, type = 'confirm' }): Promise<boolean> => {
+export const showModal: ShowModalFn = async <T = boolean | Record<string, string>>({
+  fields = [],
+  message,
+  title,
+  type = 'confirm',
+}: ModalProps): Promise<T> => {
   const template = document.querySelector<HTMLTemplateElement>('#template-modal')
   if (!template) {
     throw new Error('Modal template not found')
   }
 
-  const clone = document.importNode(template.content, true)!
-  const dialogElement = clone.querySelector('dialog')!
-  const modalBodyElement = clone.querySelector('.modal-body')!
+  const clone = document.importNode(template.content, true)
+  const dialogElement = clone.querySelector<HTMLDialogElement>('dialog')
+  const modalBodyElement = clone.querySelector<HTMLDivElement>('.modal-body')
+  const titleElement = clone.querySelector('.modal-title')
+  const actionButton = clone.querySelector('.modal-action-btn')
+  const cancelButton = clone.querySelector('.modal-cancel-btn')
 
-  clone.querySelector('.modal-title')!.textContent = title
-  clone.querySelector('.modal-action-btn')!.textContent = modalActionButtons[type] || modalActionButtons.default
-  modalBodyElement.innerHTML = `<p>${message}</p>`
-
-  if (fields?.length) {
-    fields.forEach((item) => {
-      const input = document.createElement('input')
-      input.type = 'text'
-      input.className = 'input input-bordered w-full'
-      input.name = item.name
-      input.placeholder = item.placeholder || item.title
-      modalBodyElement.appendChild(input)
-    })
+  if (!dialogElement || !modalBodyElement || !titleElement || !actionButton) {
+    throw new Error('Modal layout is incomplete')
   }
+
+  if (title) {
+    titleElement.textContent = title
+  }
+  if (message) {
+    modalBodyElement.innerHTML = `<p>${message}</p>`
+  }
+  if (fields?.length) {
+    createInputFields(fields, modalBodyElement)
+  }
+
+  actionButton.textContent = modalActionButtons[type] || modalActionButtons.default
+  dialogElement.returnValue = ReturnValue.initialValue
 
   document.body.appendChild(dialogElement)
-  if (!dialogElement) {
-    throw new Error('Dialog element not found in the template')
-  }
 
-  dialogElement.returnValue = 'initialValue'
-
-  let resolve: CallableFunction = () => null
-  let reject: CallableFunction = () => null
-  const promise = new Promise((res, rej) => {
-    resolve = res
-    reject = rej
+  cancelButton.addEventListener('click', () => {
+    dialogElement.returnValue = ReturnValue.cancel
+    dialogElement.close()
   })
 
-  dialogElement.addEventListener('close', () => {
-    const returnValue = dialogElement.returnValue
-    if (returnValue === 'ok') {
-      if (type === 'modal' || type === 'confirm') {
-        resolve(true)
-      }
-      if (type === 'prompt') {
-        const form = dialogElement.querySelector('form')!
-        const data = Object.fromEntries(new FormData(form))
+  const promise = new Promise<T>((resolve, reject) => {
+    dialogElement.addEventListener('close', () => {
+      const returnValue: ReturnValue = dialogElement.returnValue
 
-        if (Object.values(data).some((v) => !v)) {
+      switch (returnValue) {
+        case ReturnValue.ok:
+          if (type === 'modal' || type === 'confirm') {
+            resolve(true)
+          } else if (type === 'prompt') {
+            const form = dialogElement.querySelector('form')!
+            const data = extractFormData(form)
+
+            if (Object.values(data).some((value) => !value)) {
+              resolve(false)
+            } else {
+              resolve(data)
+            }
+          }
+          break
+        case ReturnValue.cancel:
+        case ReturnValue.initialValue:
           resolve(false)
-        }
-
-        resolve(data)
+          break
+        default:
+          reject(new Error(`Modal: Invalid returnValue: "${returnValue}"`))
       }
-    }
-
-    if (returnValue === 'cancel') {
-      resolve(false)
-    }
-
-    reject(new Error('Modal: Invalid logic'))
+    })
   })
 
   dialogElement.showModal()
 
-  return promise as Promise<boolean>
+  return promise
 }
