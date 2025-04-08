167237a366539d6ba1dc5766f5857d748605e309
8kto <ifhtagn@gmail.com>
2025-04-09 00:21:42 +0200

    Extend character domain with inlined attrs

--- src/js/domain/snw/Character.ts
+++ src/js/domain/snw/Character.ts
@@ -1,13 +1,30 @@
 import type { Attributes } from './Attributes'
-import type { CharacterClass } from './CharacterClass'
+import type { CharacterClassDef, CharacterRace } from './CharacterClass'
 import type { Spell } from './Magic'
+
+export type ToHit = {
+  melee: string
+  missiles: string
+}
+
+export type ArmorClass = {
+  aac: number
+  dac: number
+  armor: string | 'None'
+}
 
 export type Character = {
   gold: number
   hitPoints: number
   level: number
+  ancestry: CharacterRace
   stats: Attributes
-  characterClass: CharacterClass
+  classDef: CharacterClassDef
+  armorClass: ArmorClass
+  toHit: ToHit
+  // damage: {}
+
   spells?: Record<string, Spell> | 'All'
   prepared?: string[]
+  $isDirty?: boolean
 }
