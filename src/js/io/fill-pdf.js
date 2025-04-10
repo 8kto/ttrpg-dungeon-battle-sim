47a5deb68a3cf026467678ea59895e5a5b0fc43a
8kto <ifhtagn@gmail.com>
2025-04-10 19:40:13 +0200

    Extend list of fields

--- src/js/io/fill-pdf.js
+++ src/js/io/fill-pdf.js
@@ -12,9 +12,21 @@ const char = exportedStats.MainCharacter
  * @type {Record<string, string>}
  */
 const MAP_FIELDS_TO_CHAR_ATTRS = {
+  // Header Left
   'Class 2': 'character.classDef.name',
-  'Hit Points 2': 'character.hitPoints',
+  'Alignment 2': 'character.alignment',
+  'Ancestry 2': 'character.ancestry',
+  'LEVEL 2': 'character.level',
+  'Experience Points (XP) 2': 'character.experiencePoints',
+  'Prime Attribute 2': 'character.classDef.PrimeAttr', // array
+  'XP Bonus 2': 'character.experiencePointsBonus',
 
+  // Header right
+  'Hit Points 2': 'character.hitPoints',
+  'Saving Throw 2': 'character.classDef.SavingThrow',
+  'Armor Class 2': 'character.armorClass', // struct
+
+  // Attributes
   'Strength 2': 'character.stats.Strength.Score',
   'Dexterity 2': 'character.stats.Dexterity.Score',
   'Constitution 2': 'character.stats.Constitution.Score',
@@ -22,14 +34,20 @@ const MAP_FIELDS_TO_CHAR_ATTRS = {
   'Wisdom 2': 'character.stats.Wisdom.Score',
   'Charisma 2': 'character.stats.Charisma.Score',
 
-  'Ancestry 2': 'character.ancestry',
-  'Alignment': 'character.alignment',
-  'LEVEL 2': 'character.level',
-  'Saving Throw 2': 'character.classDef.SavingThrow',
-  'Prime Attribute': 'character.classDef.PrimeAttr', // array
-  'Armor Class 2': 'character.armorClass', // struct
-  'Experience Points (XP) 2': 'character.experiencePoints',
-  'XP Bonus 2': 'character.classDef.experiencePointsBonus',
+  // Attribute Bonuses
+  'Bonus to Hit (STR) 2': 'character.stats.Strength.ToHit',
+  'Open Doors (STR) 2': 'character.stats.Strength.Doors',
+  'Damage Bonus (STR) 2': 'character.stats.Strength.Damage',
+  'Carry Modifier (STR) 2': 'character.stats.Strength.Carry',
+  'Bonus to Missiles (DEX) 2': 'character.stats.Dexterity.MissilesToHit',
+  'Armor Bonus (DEX) 2': 'character.stats.Dexterity.ArmorClass',
+  'Hit Point Bonus (CON) 2': 'character.stats.Constitution.HitPoints',
+  'Raise Dead Survival (CON) 2': 'character.stats.Constitution.RaiseDeadSurvivalChance',
+  'Additional Languages 2': 'character.stats.Intelligence.MaxAdditionalLanguages',
+  'Max and # of Special Hirelings 2': 'character.stats.Charisma.MaxNumberOfSpecialHirelings',
+
+  // Gold & Treasure
+  'Coins 2': 'character.gold',
 }
 
 const Formatter = {
@@ -39,17 +57,30 @@ const Formatter = {
      * @returns {string}
      */
     (val) => val.snw.value,
+
   'character.armorClass':
     /**
      * @param {import('js/domain/snw/Character').ArmorClass} val
      * @returns {string}
      */
-      (val) => `${val.dac}[${val.aac}]`,
+    (val) => `${val.dac}[${val.aac}]`,
+
+  'character.classDef.PrimeAttr':
+    /**
+     * @param {import('js/domain/snw/CharacterClass').PrimeAttribute[]} val
+     * @returns {string}
+     */
+    (val) => val.map(([k]) => k.toUpperCase().slice(0, 3)).join(', '),
+
+  'character.experiencePointsBonus': (val) => `${val}%`,
+
+  'character.classDef.name': (name) => {
+    return name.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/^./, (c) => c.toUpperCase())
+  },
   default: (val) => {
     if (Array.isArray(val)) {
       return val.join(',')
-    }
-    else if (typeof val === 'object') {
+    } else if (typeof val === 'object') {
       val = JSON.stringify(val)
     }
 
@@ -59,24 +90,27 @@ const Formatter = {
 
 /**
  * @param {import('js/domain/Inventory').Character} char
- * @param {string} path
+ * @param {string} fieldName
  */
-const getAttrValue = (char, path) => {
-  const formatter = Formatter[path] ?? Formatter.default
+const getAttrValue = (char, fieldName) => {
+  const path = MAP_FIELDS_TO_CHAR_ATTRS[fieldName]
 
-  if (path?.startsWith('character.')) {
-    const chunks = path.split('.')
-    let max = chunks.length
-    let res = char
+  if (!path) {
+    0 && console.error(`Unhandled field: "${fieldName}"`)
 
-    while (max--) {
-      res = res[chunks.shift()]
-    }
-
-    return formatter(res)
+    return null
   }
 
-  return null
+  const formatter = Formatter[path] ?? Formatter.default
+  const chunks = path.split('.')
+  let max = chunks.length
+  let res = char
+
+  while (max--) {
+    res = res[chunks.shift()]
+  }
+
+  return formatter(res)
 }
 
 /**
@@ -97,12 +131,12 @@ const main = async (params) => {
 
     form.getFields().forEach((field) => {
       const fieldName = field.getName()
-      const value = getAttrValue(char, MAP_FIELDS_TO_CHAR_ATTRS[fieldName])
+      const value = getAttrValue(char, fieldName)
 
       if (value) {
         field.setText(value.toString())
       } else {
-        // f.setText(fieldName)
+        field.setText(fieldName)
       }
     })
 
