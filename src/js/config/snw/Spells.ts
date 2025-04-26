af157a7d0bc76e163f766f33a07b5ee6f048f57c
8kto <ifhtagn@gmail.com>
2025-04-26 14:05:22 +0200

    Add Cleric spells

--- src/js/config/snw/Spells.ts
+++ src/js/config/snw/Spells.ts
@@ -1,4 +1,6 @@
+import { CharacterClass } from '../../domain/snw/CharacterClass'
 import type { SpellsLevelMap } from '../../domain/snw/Spells'
+import type { CasterClass } from '../../utils/characterSheet/types'
 
 export const MagicUserSpells: SpellsLevelMap = {
   1: { 1: 1 },
@@ -22,4 +24,36 @@ export const MagicUserSpells: SpellsLevelMap = {
   19: { 1: 7, 2: 7, 3: 7, 4: 6, 5: 6, 6: 6, 7: 3, 8: 2, 9: 2 },
   20: { 1: 7, 2: 7, 3: 7, 4: 7, 5: 7, 6: 7, 7: 3, 8: 3, 9: 2 },
   21: { 1: 8, 2: 8, 3: 8, 4: 7, 5: 7, 6: 7, 7: 4, 8: 3, 9: 3 },
+}
+
+export const ClericSpells: SpellsLevelMap = {
+  1: {},
+  2: { 1: 1 },
+  3: { 1: 2 },
+  4: { 1: 2, 2: 1 },
+  5: { 1: 2, 2: 2 },
+  6: { 1: 2, 2: 2, 3: 1, 4: 1 },
+  7: { 1: 2, 2: 2, 3: 2, 4: 1, 5: 1 },
+  8: { 1: 2, 2: 2, 3: 2, 4: 2, 5: 2 },
+  9: { 1: 3, 2: 3, 3: 3, 4: 2, 5: 2 },
+  10: { 1: 3, 2: 3, 3: 3, 4: 3, 5: 3 },
+  11: { 1: 4, 2: 4, 3: 4, 4: 3, 5: 3 },
+  12: { 1: 4, 2: 4, 3: 4, 4: 4, 5: 4, 6: 1 },
+  13: { 1: 5, 2: 5, 3: 5, 4: 4, 5: 4, 6: 1 },
+  14: { 1: 5, 2: 5, 3: 5, 4: 5, 5: 5, 6: 2 },
+  15: { 1: 6, 2: 6, 3: 6, 4: 5, 5: 5, 6: 2 },
+  16: { 1: 6, 2: 6, 3: 6, 4: 6, 5: 6, 6: 3 },
+  17: { 1: 7, 2: 7, 3: 7, 4: 6, 5: 6, 6: 3, 7: 1 },
+  18: { 1: 7, 2: 7, 3: 7, 4: 7, 5: 7, 6: 4, 7: 1 },
+  19: { 1: 8, 2: 8, 3: 8, 4: 7, 5: 7, 6: 4, 7: 2 },
+  20: { 1: 8, 2: 8, 3: 8, 4: 8, 5: 8, 6: 5, 7: 2 },
+  21: { 1: 9, 2: 9, 3: 9, 4: 8, 5: 8, 6: 5, 7: 3 },
+}
+
+export const DruidSpells = {}
+
+export const CasterSpellsMap: Record<CasterClass, SpellsLevelMap> = {
+  [CharacterClass.MagicUser]: MagicUserSpells,
+  [CharacterClass.Cleric]: ClericSpells,
+  [CharacterClass.Druid]: DruidSpells,
 }
