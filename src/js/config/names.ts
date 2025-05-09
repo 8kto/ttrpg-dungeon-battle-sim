27d5bc325ebd0d22845e4a7ad030f94ff8503e6e
8kto <ifhtagn@gmail.com>
2025-05-09 18:22:04 +0200

    Add NameProvider

---
+++ src/js/config/names.ts
@@ -0,0 +1,20 @@
+export const PlayerNamesChunks = [
+  ['Jer', 'nek'],
+  ['Zde', 'rek'],
+  ['Ma', 'vek'],
+  ['Iva', 'lok'],
+  ['Ku', 'dor'],
+  ['El', 'kar'],
+  ['O', 'na'],
+]
+
+export const MonsterNamesChunks = [
+  ['Xul', 'qua', 'sha'],
+  ['Qua', 'shi', 'nia'],
+  ['Za', 'xul', 'atl'],
+  ['Ura', 'zu', 'rra'],
+  ['Az', 'al', 'bak'],
+  ['Xe', 'na', 'tor'],
+  ['El', 'chu', 'bra'],
+  ['Ge', '', 'us'],
+]
